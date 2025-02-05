1340380378edf76ea9725c7128ec6d6395ce7c2e
Oyudi22 <ochiaifelipe@gmail.com>
2025-02-05 01:12:41 -0300

    updating the scripts

--- ClickerGame/scripts/main.js
+++ ClickerGame/scripts/main.js
@@ -4,7 +4,6 @@ let clicksPower = 1;
 const button = document.getElementById('buttonPoints');
 const points = document.getElementById('points');
 const clickingPower = document.getElementById('clickPower');
-const upgrades = document.getElementById('upgrades');
 const upgrade1 = document.getElementById('upgrade1');
 const upgrade2 = document.getElementById('upgrade2');
 
@@ -14,11 +13,13 @@ let upgrade2Price = 500;
 let upgrade2Level = 1;
 
 function updateUI(){
-    points.innerHTML = clicks;
-    clickingPower.innerHTML = `Click Power: ${clicksPower}`;
-
-    upgrade1.innerHTML = `+${2 + (upgrade1Level - 1) * 2} points per click <p>(Lvl ${upgrade1Level} - ${upgrade1Price} points)</p>`;
-    upgrade2.innerHTML = `Multiply ${1.5 + (upgrade2Level - 1) * 0.1} Click Power <p>(Lvl ${upgrade2Level} - ${upgrade2Price} points)</p>`;
+    points.textContent = Math.round(clicks.toFixed(1));
+    clickingPower.textContent = `Click Power: ${Math.round(clicksPower.toFixed(1))}`;
+    
+    //upgrade1.innerHTML = `+${2 + (upgrade1Level - 1) * 2} points per click <br><p>(Lvl ${upgrade1Level} - ${upgrade1Price} points)</p>`;
+    //upgrade2.innerHTML = `Multiply ${(1.5 + (upgrade2Level - 1) * 0.1).toFixed(2)} Click Power <br><p>(Lvl ${upgrade2Level} - ${upgrade2Price} points)</p>`;
+    document.getElementById('upgrade1').innerHTML = `+${2 + (upgrade1Level - 1) * 2} points per click <br><p>(Lvl ${upgrade1Level} - ${upgrade1Price} points)</p>`;
+    document.getElementById('upgrade2').innerHTML = `Multiply ${Math.round(1.5 + (upgrade2Level - 1) * 0.1).toFixed(2)} Click Power <br><p>(Lvl ${upgrade2Level} - ${upgrade2Price} points)</p>`;
 
     checkUpgrades();
 }
@@ -34,6 +35,7 @@ upgrade1.addEventListener('click', () => {
         clicksPower += 2 + (upgrade1Level - 1) * 2;
         upgrade1Level++;
         upgrade1Price = Math.floor(upgrade1Price * 1.2);
+
         updateUI();
     }
     
@@ -42,9 +44,10 @@ upgrade1.addEventListener('click', () => {
 upgrade2.addEventListener('click', () => {
     if(clicks >= upgrade2Price){
         clicks -= upgrade2Price;
-        clicksPower *= 1.5 + (upgrade2Level - 1) * 0.1;
+        clicksPower *= 1.5 + (upgrade2Level - 1) * 0.5;
         upgrade2Level++;
         upgrade2Price = Math.floor(upgrade2Price * 1.2);
+
         updateUI();
     }
 });
